2f1193e3d7375519873b3145ff2a224361d90a6f
Piyush Verma <piyushverma0919@gamil.com>
2025-03-07 20:22:07 +0530

    REACT BASICS, USESTATE

---
+++ 01React/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+
+
+import Youtube from "./Youtube.jsx"
+function App() {
+  
+
+  return (
+    <>
+    <h1>React with vite | PV</h1>
+    <h1>React with vite | PV</h1>
+    <Youtube></Youtube>
+    </>
+  )
+}
+
+export default App
---
+++ 02Counter/02Counter/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // let counter = 10
+ let [counter, setCounter] =   useState(15)
+  
+  
+const addValue = ()=>{
+if(counter>=20) return
+  setCounter(counter+1)
+  console.log("Clicked",counter);
+}
+
+const removeValue = ()=>{
+if(counter<=0) return
+  setCounter(counter-1)
+}
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      <h2>Counter Value:{counter}</h2>
+
+      <button
+      onClick={addValue}
+      >Add Value:{counter}</button>
+      <br />
+      <button
+      onClick={removeValue}>remove Value:{counter}</button>
+   
+    </>
+  )
+}
+
+export default App
